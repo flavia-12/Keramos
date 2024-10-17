3b158c8de85853a7303831c827d07dc73f3f4b05
Flavia Felice <flaviafelice@MacBook-Pro-di-Flavia.local>
2024-10-17 19:37:43 +0200

    landing page complete

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
-var multipleCardCarousel = document.querySelector(
-    "#carouselExampleControls"
-  );
-  if (window.matchMedia("(min-width: 768px)").matches) {
-    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-      interval: false,
-    });
-    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-    var cardWidth = $(".carousel-item").width();
-    var scrollPosition = 0;
-    $("#carouselExampleControls .carousel-control-next").on("click", function () {
-      if (scrollPosition < carouselWidth - cardWidth) {
-        scrollPosition += cardWidth;
-        $("#carouselExampleControls .carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
+
+
+  function reveal() {
+    var reveals = document.querySelectorAll(".fade-left, .fade-right, .fade-up, .fade-down"); // Seleziona gli elementi con classi specifiche
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight; // Altezza della finestra
+      var elementTop = reveals[i].getBoundingClientRect().top; // Posizione dell'elemento
+      var elementVisible = 150; // Quanto deve essere visibile per attivare l'animazione
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active"); // Aggiunge la classe "active" per attivare l'animazione
+      } else {
+        reveals[i].classList.remove("active"); // Rimuove la classe "active" se non è visibile
       }
-    });
-    $("#carouselExampleControls .carousel-control-prev").on("click", function () {
-      if (scrollPosition > 0) {
-        scrollPosition -= cardWidth;
-        $("#carouselExampleControls .carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
-      }
-    });
-  } else {
-    $(multipleCardCarousel).addClass("slide");
+    }
   }
+  
+  window.addEventListener("scroll", reveal); // Ascolta l'evento di scroll
+  reveal(); // Esegui la funzione all'apertura della pagina
+  
 
 
   
